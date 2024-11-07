ca85c5e8aa751b09a70d8d1d9326963481184c97
Joel Granados <joel.granados@kernel.org>
2024-11-07 10:00:46 +0100

    Add libvfn as a dependency
    For now we just point it to a ref. We should change this to a "proper"
    tag when when we have a better idea on where things will go.
    
    Signed-off-by: Joel Granados <joel.granados@kernel.org>

--- flake.nix
+++ flake.nix
@@ -1,14 +1,21 @@
 {
   description = "iommutests flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    libvfn = {
+      url = "github:Joelgranados/libvfn/d7756ae53b8fd2e2dbc8b1b972fb3736d122906d";
+    };
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, libvfn, ... }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
+      system = "x86_64-linux";
     in {
-      devShells.x86_64-linux.default = pkgs.mkShell {
+      devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
+          libvfn.packages.${system}.default
           meson
           ninja
           git
@@ -21,4 +28,3 @@
       };
     };
 }
-
